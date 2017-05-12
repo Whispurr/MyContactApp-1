eb91dcd641dfad24f2ad1190ce3f485825305fc7
Daniel Wang <danielwang243@gmail.com>
2017-05-12 10:41:37 -0700

    added field and button, finished database helper

--- MyContactApp/app/src/main/java/org/beautiful_butterflies/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/org/beautiful_butterflies/mycontactapp/MainActivity.java
@@ -2,12 +2,20 @@ package org.beautiful_butterflies.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
     }
 }
