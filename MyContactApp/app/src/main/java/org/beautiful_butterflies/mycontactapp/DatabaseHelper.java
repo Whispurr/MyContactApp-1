834a05dba3fa0428d81ca0beee76c9108463d3f9
Daniel Wang <danielwang243@gmail.com>
2017-05-16 09:58:50 -0700

    added two more db fields/buttons

--- MyContactApp/app/src/main/java/org/beautiful_butterflies/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/org/beautiful_butterflies/mycontactapp/DatabaseHelper.java
@@ -7,35 +7,39 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Contact.db";
-
     public static final String TABLE_NAME = "contact_table";
-
-    public static final String COL_1 = "ID";
-
-    public static final String COL_2 = "NAME";
-
+    public static final String col_1 = "ID";
+    public static final String col_2 = "NAME";
+    public static final String col_3 = "AGE";
+    public static final String col_4 = "ADDRESS";
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE TEXT, ADDRESS TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
         onCreate(db);
     }
-
-    public boolean insertData(String name) {
+    public boolean insertData(String name, String age, String address) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2, name);
-
-        long result = db.insert(TABLE_NAME, null, contentValues);
-        if(result == -1) return false;
-        else return true;
+        contentValues.put(col_2,name);
+        contentValues.put(col_3,age);
+        contentValues.put(col_4,address);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result == -1 ) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
--- MyContactApp/app/src/main/java/org/beautiful_butterflies/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/org/beautiful_butterflies/mycontactapp/MainActivity.java
@@ -6,11 +6,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddData;
 
     @Override
@@ -19,21 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
-
-        //Add the layout vars
-        editName = (EditText) findViewById(R.id.editText_name);
+        //add the layout bars
+        editName = (EditText) findViewById(R.id.editText_Name);
+        editAge = (EditText) findViewById(R.id.editText_Age);
+        editAddress = (EditText) findViewById(R.id.editText_address);
     }
-
     public void addData(View v) {
-        boolean isInserted = myDb.insertData(editName.getText().toString());
+        boolean isInserted = (myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString()));
 
-        if(isInserted) {
+        String text = "";
+        if (isInserted == true) {
             Log.d("MyContact", "Success inserting data");
-            //Insert toast here...
+            text = "data inserted";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
+            //insert toast message here...
         }
         else {
-            Log.d("MyContact", "Failure inserting data");
-            //Insert toast here...
+            text = "data failure";
+            Toast toast = Toast.makeText(getApplicationContext(), text,Toast.LENGTH_SHORT);
+            toast.show();
+            Log.d("MyContact", "SFailer inserting data");
+            //insert toast message here...
         }
 
     }
